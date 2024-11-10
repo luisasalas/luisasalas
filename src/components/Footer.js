a353162375a0e696b51bd0e7002a5990099c4608
Luis Salas <luiscorona193@gmail.com>
2024-11-10 03:06:21 -0800

    navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import InstagramIcon from '@material-ui/icons/Instagram';
+import GitlabIcon from '../images/gitlab-logo-500.svg';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
@@ -31,16 +31,20 @@ function Footer() {
                 className={classes.root}
                 style={{ padding: 0 }}
                 icon={<LinkedInIcon />}
+                href='https://www.linkedin.com/in/luis-salascorona/'
             />
             <BottomNavigationAction
                 className={classes.root}
                 style={{ padding: 0 }}
                 icon={<GitHubIcon />}
+                href='https://github.com/luisasalas'
             />
             <BottomNavigationAction
                 style={{ padding: 0 }}
                 className={classes.root}
-                icon={<InstagramIcon />}
+                icon={<GitlabIcon />}
+                href='https://gitlab.com/luiscorona193'
+                
             />
         </BottomNavigation>
     )
